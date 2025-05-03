3ed9dc7d4fba9f4d5a305d495f0e4e512a4fd8ae
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-05-03 02:28:27 +0000

    Implemented Grin.java

--- src/main/java/edu/grinnell/csc207/compression/Grin.java
+++ src/main/java/edu/grinnell/csc207/compression/Grin.java
@@ -1,19 +1,35 @@
 package edu.grinnell.csc207.compression;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * The driver for the Grin compression program.
  */
 public class Grin {
+    private static final int MAGIC_NUMBER = 0xFACEB00C;
     /**
      * Decodes the .grin file denoted by infile and writes the output to the
      * .grin file denoted by outfile.
-     * @param infile the file to decode
+     * @param infile the file to
+     * @throws IOException  decode
      * @param outfile the file to ouptut to
      */
-    public static void decode (String infile, String outfile) {
-        // TODO: fill me in!
+    public static void decode (String infile, String outfile) throws IOException {
+        BitInputStream in = new BitInputStream(infile);
+        BitOutputStream out = new BitOutputStream(outfile);
+    
+        int magicNumber = in.readBits(32);
+        if (magicNumber != MAGIC_NUMBER) {
+            in.close();
+            out.close();
+            throw new IllegalArgumentException("Invalid .grin file: incorrect magic number");
+        }
+        HuffmanTree tree = new HuffmanTree(in);
+        tree.decode(in, out);
+        in.close();
+        out.close();
     }
 
     /**
@@ -22,10 +38,18 @@ public class Grin {
      * BitInputStream, consuming 8 bits at a time.
      * @param file the file to read
      * @return a freqency map for the given file
+     * @throws IOException 
      */
-    public static Map<Short, Integer> createFrequencyMap (String file) {
-        // TODO: fill me in!
-        return null;
+    public static Map<Short, Integer> createFrequencyMap (String file) throws IOException {
+        Map<Short, Integer> freqMap = new HashMap<>();
+        BitInputStream in = new BitInputStream(file);
+        int value;
+        while ((value = in.readBits(8)) != -1) {
+            short key = (short) value;
+            freqMap.put(key, freqMap.getOrDefault(key, 0) + 1);
+        }
+        in.close();
+        return freqMap;
     }
 
     /**
@@ -34,16 +58,43 @@ public class Grin {
      * @param infile the file to encode.
      * @param outfile the file to write the output to.
      */
-    public static void encode(String infile, String outfile) {
-        // TODO: fill me in!
+
+    public static void encode(String infile, String outfile) throws IOException {
+        Map<Short, Integer> freqMap = createFrequencyMap(infile);
+        HuffmanTree tree = new HuffmanTree(freqMap);
+        BitInputStream in = new BitInputStream(infile);
+        BitOutputStream out = new BitOutputStream(outfile);
+        out.writeBits(MAGIC_NUMBER, 32);
+        tree.serialize(out);
+        tree.encode(in, out);
+        in.close();
+        out.close();
     }
+    
 
     /**
      * The entry point to the program.
      * @param args the command-line arguments.
+     * @throws IOException 
      */
-    public static void main(String[] args) {
-        // TODO: fill me in!
-        System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+            return;
+        }
+        String operation = args[0];
+        String infile = args[1];
+        String outfile = args[2];
+    
+        if (operation.equals("encode")) {
+            encode(infile, outfile);
+            System.out.println("Successfully encoded " + infile + " to " + outfile);
+        } else if (operation.equals("decode")) {
+            decode(infile, outfile);
+            System.out.println("Successfully decoded " + infile + " to " + outfile);
+        } else {
+            System.out.println("Invalid operation: " + operation);
+            System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+        }
     }
 }
